f82ef89abbb2e1928bcc2525389aab28d84f26a7
Adarsh Gupta <adarxxxshgupta@gmail.com>
2026-06-22 11:20:44 +0530

    fix: update pdf_service.py

--- app/services/pdf_service.py
+++ app/services/pdf_service.py
@@ -1,13 +1,7 @@
-import pdfplumber
 import io
+from pdfminer.high_level import extract_text
 
 def extract_text_from_pdf(file_bytes: bytes):
     pdf_file = io.BytesIO(file_bytes)
-
-    with pdfplumber.open(pdf_file) as pdf:
-        text = ""
-        for page in pdf.pages:
-            page_text = page.dedupe_chars(tolerance=1).extract_text() or ""
-            text += page_text
-
+    text = extract_text(pdf_file)
     return text
